bb02284ce1bfd912d4aad395aef135b31ed3f056
kunal-geeks <kunal.ucet@gmail.com>
2023-03-16 09:24:21 +0530

    printing final report from a book

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+with open('books/frankenstein.txt') as f:
+    file_contents = f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+    
+def count_letters(text):
+    words = text.split()
+    letters_count = {}
+    for letter in range(97,123):
+        letters_count[chr(letter)] = 0
+    for word in words:
+        for letter in word.lower():
+            if letter in letters_count:
+                letters_count[letter] += 1
+    return letters_count
+
+def print_report():
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"The frankenstein book has a total of {count_words(file_contents)} words!\n")
+    letters_count = count_letters(file_contents)
+    for letter in letters_count:
+        print(f"The '{letter}' character was found {letters_count[letter]} times")
+    print("\n--- End report ---")
+
+def main():
+    print_report()
+    
+main()
